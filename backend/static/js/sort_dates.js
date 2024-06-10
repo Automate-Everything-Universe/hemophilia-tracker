e1c32f0ee43b26203a643201023096fc872709e2
Dragos Josan <dragosjosan@gmail.com>
2024-06-10 22:03:32 +0200

    Fix date sorting function

--- backend/static/js/sort_dates.js
+++ backend/static/js/sort_dates.js
@@ -1,16 +1,24 @@
 function sortDates() {
-    const dayOrder = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"];
+    const dayOrder = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    const splitTimeString = (timeString) => {
+        const [day, time, period] = timeString.split(/[\s:]+/);
+        return { day, time, period };
+    };
 
     dates.sort((a, b) => {
-        const [dayA, timeA, periodA] = a.split(/[\s:]+/);
-        const [dayB, timeB, periodB] = b.split(/[\s:]+/);
-        let timeAandPeriodA = timeA + ":" + periodA;
-        let timeBandPeriodB = timeB + ":" + periodB;
-        const [hourA, minuteA] = timeAandPeriodA.split(":").map(Number);
-        const [hourB, minuteB] = timeBandPeriodB.split(":").map(Number);
+        const { day: dayA, time: timeA, period: periodA } = splitTimeString(a);
+        const { day: dayB, time: timeB, period: periodB } = splitTimeString(b);
 
-        const totalMinutesA = (dayOrder.indexOf(dayA) * 24 * 60) + ((periodA === "PM" && hourA !== 12 ? hourA + 12 : (periodA === "AM" && hourA === 12 ? 0 : hourA)) * 60) + minuteA;
-        const totalMinutesB = (dayOrder.indexOf(dayB) * 24 * 60) + ((periodB === "PM" && hourB !== 12 ? hourB + 12 : (periodB === "AM" && hourB === 12 ? 0 : hourB)) * 60) + minuteB;
+        const [hourA, minuteA] = timeA.split(":").map(Number);
+        const [hourB, minuteB] = timeB.split(":").map(Number);
+
+        const totalMinutesA = (dayOrder.indexOf(dayA) * 24 * 60) +
+                              ((periodA === "PM" && hourA !== 12 ? hourA + 12 : (periodA === "AM" && hourA === 12 ? 0 : hourA)) * 60) +
+                              minuteA;
+        const totalMinutesB = (dayOrder.indexOf(dayB) * 24 * 60) +
+                              ((periodB === "PM" && hourB !== 12 ? hourB + 12 : (periodB === "AM" && hourB === 12 ? 0 : hourB)) * 60) +
+                              minuteB;
 
         return totalMinutesA - totalMinutesB;
     });
